edeb3644f7762cb32b7d4660addf74238f718dad
Frazer Smith <frazer.dev@outlook.com>
2022-01-14 15:42:14 +0000

    chore(routes/contact): finish up schema

--- src/routes/contact/schema.js
+++ src/routes/contact/schema.js
@@ -2,6 +2,53 @@ const S = require("fluent-json-schema");
 
 const tags = ["Community Contacts"];
 
+const contactBaseSchema = S.object()
+	.prop("id", S.string().format("uuid"))
+	.prop(
+		"match",
+		S.object()
+			.prop(
+				"type",
+				S.string().examples(["gp_id", "postcode", "school code"])
+			)
+			.prop("value", S.string().examples(["BA229RZ"]))
+			.prop("receiver", S.string().examples(["Sherborne"]))
+	)
+	.prop(
+		"telecom",
+		S.array()
+			.items(
+				S.object()
+					.prop("system", S.string().examples(["email"]))
+					.prop("value", S.string().examples(["example@ydh.nhs.uk"]))
+					.prop(
+						"use",
+						S.string().examples([
+							"Community Midwives",
+							"Health Visitors",
+						])
+					)
+			)
+			.uniqueItems(true)
+	)
+	.prop(
+		"meta",
+		S.object()
+			.prop(
+				"created",
+				S.string()
+					.examples(["2022-01-13T14:05:54.000Z"])
+					.format("date-time")
+			)
+			.prop(
+				"last_updated",
+				S.string()
+					.examples(["2022-01-13T14:05:54.000Z"])
+					.format("date-time")
+			)
+	)
+	.required(["match", "telecom"]);
+
 /**
  * Fastify uses AJV for JSON Schema Validation,
  * see https://www.fastify.io/docs/latest/Validation-and-Serialization/
@@ -61,58 +108,7 @@ const contactGetReadSchema = {
 		)
 		.required(["id"]),
 	response: {
-		200: S.object()
-			.prop("id", S.string().format("uuid"))
-			.prop(
-				"match",
-				S.object()
-					.prop(
-						"type",
-						S.string().examples([
-							"gp_id",
-							"postcode",
-							"school code",
-						])
-					)
-					.prop("value", S.string().examples(["BA229RZ"]))
-					.prop("receiver", S.string().examples(["Sherborne"]))
-			)
-			.prop(
-				"telecom",
-				S.array()
-					.items(
-						S.object()
-							.prop("system", S.string().examples(["email"]))
-							.prop(
-								"value",
-								S.string().examples(["example@ydh.nhs.uk"])
-							)
-							.prop(
-								"use",
-								S.string().examples([
-									"Community Midwives",
-									"Health Visitors",
-								])
-							)
-					)
-					.uniqueItems(true)
-			)
-			.prop(
-				"meta",
-				S.object()
-					.prop(
-						"created",
-						S.string()
-							.examples(["2022-01-13T14:05:54.000Z"])
-							.format("date-time")
-					)
-					.prop(
-						"last_updated",
-						S.string()
-							.examples(["2022-01-13T14:05:54.000Z"])
-							.format("date-time")
-					)
-			),
+		200: contactBaseSchema,
 		401: S.ref("responses#/definitions/unauthorized").description(
 			"Unauthorized"
 		),
@@ -134,14 +130,124 @@ const contactGetReadSchema = {
 	},
 };
 
-// TODO: add 200 response
 const contactGetSearchSchema = {
 	tags,
 	summary: "Search community contact",
 	description: "Return community contact records.",
 	operationId: "getSearchContact",
 	produces: ["application/json"],
+	query: S.object()
+		.additionalProperties(false)
+		.prop(
+			"match.type",
+			S.string()
+				.description("Type of matching value")
+				.examples(["gp_id", "postcode", "school code"])
+		)
+		.prop(
+			"match.value",
+			S.string()
+				.description("Matching value, supports `*` wildcards")
+				.examples(["BA229RZ", "BA2*", "BA22*"])
+		)
+		.prop(
+			"match.receiver",
+			S.string()
+				.description("Receiving organisation or area")
+				.examples(["Sherborne", "Springmead Surgery, Chard"])
+		)
+		.prop(
+			"telecom.value",
+			S.string().description().examples(["example@ydh.nhs.uk"])
+		)
+		.prop(
+			"lastModified",
+			S.anyOf([
+				S.string()
+					.description(
+						"Last modified datetime of community contact record"
+					)
+					.examples([
+						"2022-01-13",
+						"ge2022-01-13T00:00:01",
+						"ge2022-01-13",
+						"2022-01-13T00:00:01",
+					])
+					.pattern(
+						/^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}|)$/m
+					),
+				S.array()
+					.items(
+						S.string()
+							.description(
+								"Last modified datetime of community contact record"
+							)
+							.examples([
+								"2022-01-13",
+								"ge2022-01-13T00:00:01",
+								"ge2022-01-13",
+								"2022-01-13T00:00:01",
+							])
+							.pattern(
+								/^(?:eq|ne|ge|le|gt|lt|sa|eb|ap|)\d{4}-\d{2}-\d{2}(?:T\d{2}:\d{2}:\d{2}|)$/m
+							)
+					)
+					.minItems(2)
+					.maxItems(2)
+					.uniqueItems(true),
+			])
+		)
+		.prop(
+			"page",
+			S.number()
+				.description("Page to retrieve")
+				.default(1)
+				.examples([1, 10])
+				.minimum(1)
+		)
+		.prop(
+			"perPage",
+			S.number()
+				.description(
+					"Number of community contact records to return per page"
+				)
+				.default(1)
+				.examples([1, 10])
+				.minimum(1)
+				.maximum(100)
+		),
 	responses: {
+		200: S.object()
+			.prop("link", S.string().format("uri"))
+			.prop(
+				"entry",
+				S.array().items(
+					S.object()
+						.prop("url", S.string().format("uri"))
+						.extends(contactBaseSchema)
+				)
+			)
+			.prop(
+				"meta",
+				S.object().prop(
+					"pagination",
+					S.object()
+						.prop("total", S.number().examples([0, 1, 10]))
+						.prop(
+							"per_page",
+							S.number()
+								.default(1)
+								.examples([1, 10])
+								.minimum(1)
+								.maximum(100)
+						)
+						.prop(
+							"current_page",
+							S.number().default(1).examples([1, 10]).minimum(1)
+						)
+						.prop("total_pages", S.number().examples([1, 10]))
+				)
+			),
 		401: S.ref("responses#/definitions/unauthorized").description(
 			"Unauthorized"
 		),
@@ -168,41 +274,9 @@ const contactPostSchema = {
 	summary: "Add community contact",
 	description: "Add a new community contact record.",
 	operationId: "postContact",
-	body: S.object()
-		.prop(
-			"match",
-			S.object()
-				.prop(
-					"type",
-					S.string().examples(["gp_id", "postcode", "school code"])
-				)
-				.prop("value", S.string().examples(["BA229RZ"]))
-				.prop("receiver", S.string().examples(["Sherborne"]))
-				.required("type", "value", "receiver")
-		)
-		.prop(
-			"telecom",
-			S.array()
-				.items(
-					S.object()
-						.prop("system", S.string().examples(["email"]))
-						.prop(
-							"value",
-							S.string().examples(["example@ydh.nhs.uk"])
-						)
-						.prop(
-							"use",
-							S.string().examples([
-								"Community Midwives",
-								"Health Visitors",
-							])
-						)
-						.required("system", "value", "use")
-				)
-				.minItems(1)
-				.uniqueItems(true)
-		)
-		.required(["match", "telecom"]),
+	body: contactBaseSchema
+		.additionalProperties(false)
+		.only("match", "telecom"),
 	response: {
 		204: S.string().raw({ nullable: true }).description("No Content"),
 		401: S.ref("responses#/definitions/unauthorized").description(
@@ -223,7 +297,6 @@ const contactPostSchema = {
 	},
 };
 
-// TODO: add body and responses
 const contactPutSchema = {
 	tags,
 	summary: "Update community contact",
@@ -238,6 +311,27 @@ const contactPutSchema = {
 				.format("uuid")
 		)
 		.required(["id"]),
+	body: contactBaseSchema
+		.additionalProperties(false)
+		.only("match", "telecom"),
+	response: {
+		204: S.string().raw({ nullable: true }).description("No Content"),
+		401: S.ref("responses#/definitions/unauthorized").description(
+			"Unauthorized"
+		),
+		406: S.ref("responses#/definitions/notAcceptable").description(
+			"Not Acceptable"
+		),
+		429: S.ref("responses#/definitions/tooManyRequests").description(
+			"Too Many Requests"
+		),
+		500: S.ref("responses#/definitions/internalServerError").description(
+			"Internal Server Error"
+		),
+		503: S.ref("responses#/definitions/serviceUnavailable").description(
+			"Service Unavailable"
+		),
+	},
 };
 
 module.exports = {
