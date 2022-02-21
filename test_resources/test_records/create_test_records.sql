7470ea5c75c155e2b7f5fd3f6b62b7d83149d80e
Frazer Smith <frazer.dev@outlook.com>
2022-02-21 11:57:43 +0000

    style: remove trailing whitespace

--- test_resources/test_records/create_test_records.sql
+++ test_resources/test_records/create_test_records.sql
@@ -1,4 +1,4 @@
-INSERT INTO lookup.contacts 
+INSERT INTO lookup.contacts
     (match_type, match_value, match_receiver, telecom)
 VALUES
     ('postcode', 'TA126JU', 'Area North', '[{ "system": "email", "value": "examplehv@nhs.net", "use": "Health Visitors"}, { "system": "email", "value": "examplemidwife@ydh.nhs.uk", "use": "Community Midwives"}]'),
