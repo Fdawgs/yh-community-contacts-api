cb15fd97edbe470b1f76761b5597e042b286fdb6
Frazer Smith <frazer.dev@outlook.com>
2022-03-24 12:45:35 +0000

    build(deps-dev): replace `faker` with `@faker-js/faker`

--- src/routes/contact/route.test.js
+++ src/routes/contact/route.test.js
@@ -1,4 +1,4 @@
-const faker = require("faker");
+const { faker } = require("@faker-js/faker");
 const Fastify = require("fastify");
 const sensible = require("fastify-sensible");
 const route = require(".");
