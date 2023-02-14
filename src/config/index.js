fea96848d73d87e184783316fca07f61891fd287
Frazer Smith <frazer.dev@outlook.com>
2023-02-14 08:54:10 +0000

    style(config): use consistent style across api repos

--- src/config/index.js
+++ src/config/index.js
@@ -95,7 +95,7 @@ async function getConfig() {
 			.prop("LOG_ROTATION_MAX_LOGS", S.anyOf([S.string(), S.null()]))
 			.prop("LOG_ROTATION_MAX_SIZE", S.anyOf([S.string(), S.null()]))
 
-			// Process Load Handling
+			// Process load handling
 			.prop(
 				"PROC_LOAD_MAX_EVENT_LOOP_DELAY",
 				S.anyOf([S.number(), S.null()])
@@ -110,7 +110,7 @@ async function getConfig() {
 			)
 			.prop("PROC_LOAD_MAX_RSS_BYTES", S.anyOf([S.number(), S.null()]))
 
-			// Rate Limiting
+			// Rate limiting
 			.prop("RATE_LIMIT_EXCLUDED_ARRAY", S.anyOf([S.string(), S.null()]))
 			.prop(
 				"RATE_LIMIT_MAX_CONNECTIONS_PER_MIN",
@@ -124,7 +124,7 @@ async function getConfig() {
 			// Bearer token auth
 			.prop("BEARER_TOKEN_AUTH_ENABLED", S.anyOf([S.boolean(), S.null()]))
 
-			// Database Connection
+			// Database connection
 			.prop(
 				"DB_CLIENT",
 				S.anyOf([S.string().enum(["mssql", "postgresql"]), S.null()])
@@ -181,6 +181,22 @@ async function getConfig() {
 			maxAge: env.CORS_MAX_AGE || null,
 			origin: parseCorsParameter(env.CORS_ORIGIN) || false,
 		},
+		processLoad: {
+			maxEventLoopDelay: env.PROC_LOAD_MAX_EVENT_LOOP_DELAY || 0,
+			maxEventLoopUtilization:
+				env.PROC_LOAD_MAX_EVENT_LOOP_UTILIZATION || 0,
+			maxHeapUsedBytes: env.PROC_LOAD_MAX_HEAP_USED_BYTES || 0,
+			maxRssBytes: env.PROC_LOAD_MAX_RSS_BYTES || 0,
+		},
+		rateLimit: {
+			allowList: env.RATE_LIMIT_EXCLUDED_ARRAY
+				? secJSON.parse(env.RATE_LIMIT_EXCLUDED_ARRAY)
+				: null,
+			continueExceeding: true,
+			hook: "onSend",
+			max: env.RATE_LIMIT_MAX_CONNECTIONS_PER_MIN || 1000,
+			timeWindow: 60000,
+		},
 		helmet: {
 			contentSecurityPolicy: {
 				directives: {
@@ -208,22 +224,6 @@ async function getConfig() {
 			// Only supported by Chrome at time of writing
 			originAgentCluster: false,
 		},
-		processLoad: {
-			maxEventLoopDelay: env.PROC_LOAD_MAX_EVENT_LOOP_DELAY || 0,
-			maxEventLoopUtilization:
-				env.PROC_LOAD_MAX_EVENT_LOOP_UTILIZATION || 0,
-			maxHeapUsedBytes: env.PROC_LOAD_MAX_HEAP_USED_BYTES || 0,
-			maxRssBytes: env.PROC_LOAD_MAX_RSS_BYTES || 0,
-		},
-		rateLimit: {
-			allowList: env.RATE_LIMIT_EXCLUDED_ARRAY
-				? secJSON.parse(env.RATE_LIMIT_EXCLUDED_ARRAY)
-				: null,
-			continueExceeding: true,
-			hook: "onSend",
-			max: env.RATE_LIMIT_MAX_CONNECTIONS_PER_MIN || 1000,
-			timeWindow: 60000,
-		},
 		swagger: {
 			openapi: {
 				info: {
@@ -247,7 +247,7 @@ async function getConfig() {
 							"Yeovil District Hospital NHS Foundation Trust Logo",
 					},
 				},
-				// Components object always populated by shared schemas at launch
+				// Components object populated by shared schemas at launch
 				components: {
 					securitySchemes: {
 						basicAuth: {
@@ -285,31 +285,6 @@ async function getConfig() {
 	// Ensure API listens on both IPv4 and IPv6 addresses if not explicitly set
 	if (env.HOST) {
 		config.fastify.host = env.HOST;
-	}
-
-	if (env.LOG_ROTATION_FILENAME) {
-		const logFile = path.normalizeTrim(env.LOG_ROTATION_FILENAME);
-
-		// Rotation options: https://github.com/rogerc/file-stream-rotator/#options
-		config.fastifyInit.logger.stream = rotatingLogStream.getStream({
-			audit_file: path.joinSafe(path.dirname(logFile), ".audit.json"),
-			date_format: env.LOG_ROTATION_DATE_FORMAT || "YYYY-MM-DD",
-			filename: logFile,
-			frequency: env.LOG_ROTATION_FREQUENCY || "daily",
-			max_logs: env.LOG_ROTATION_MAX_LOGS,
-			size: env.LOG_ROTATION_MAX_SIZE,
-			verbose: false,
-		});
-	}
-
-	if (env.BEARER_TOKEN_AUTH_ENABLED === true) {
-		config.swagger.openapi.components.securitySchemes.bearerToken = {
-			type: "http",
-			description:
-				"Expects the request to contain an `Authorization` header with a bearer token.",
-			scheme: "bearer",
-			bearerFormat: "Bearer <token>",
-		};
 	}
 
 	// Enable HTTPS using cert/key or passphrase/pfx combinations
@@ -351,6 +326,33 @@ async function getConfig() {
 	if (config.fastifyInit.https && env.HTTPS_HTTP2_ENABLED === true) {
 		config.fastifyInit.https.allowHTTP1 = true;
 		config.fastifyInit.http2 = true;
+	}
+
+	// Set Pino transport
+	if (env.LOG_ROTATION_FILENAME) {
+		const logFile = path.normalizeTrim(env.LOG_ROTATION_FILENAME);
+
+		// Rotation options: https://github.com/rogerc/file-stream-rotator/#options
+		config.fastifyInit.logger.stream = rotatingLogStream.getStream({
+			audit_file: path.joinSafe(path.dirname(logFile), ".audit.json"),
+			date_format: env.LOG_ROTATION_DATE_FORMAT || "YYYY-MM-DD",
+			filename: logFile,
+			frequency: env.LOG_ROTATION_FREQUENCY || "daily",
+			max_logs: env.LOG_ROTATION_MAX_LOGS,
+			size: env.LOG_ROTATION_MAX_SIZE,
+			verbose: false,
+		});
+	}
+
+	// Bearer token auth
+	if (env.BEARER_TOKEN_AUTH_ENABLED === true) {
+		config.swagger.openapi.components.securitySchemes.bearerToken = {
+			type: "http",
+			description:
+				"Expects the request to contain an `Authorization` header with a bearer token.",
+			scheme: "bearer",
+			bearerFormat: "Bearer <token>",
+		};
 	}
 
 	return config;
