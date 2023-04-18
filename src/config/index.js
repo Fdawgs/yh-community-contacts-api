890780d0161d8d1553eacd00abf756e618232ec3
Frazer Smith <frazer.dev@outlook.com>
2023-04-18 13:42:41 +0100

    chore: replace ydh nhsft branding and links with somerset nhsft

--- src/config/index.js
+++ src/config/index.js
@@ -232,7 +232,7 @@ async function getConfig() {
 				info: {
 					title: "Community Contacts API",
 					description:
-						'<a href="https://yeovilhospital.co.uk/">Yeovil District Hospital NHSFT</a>\'s Community Contacts RESTful API, a Node.js application using the <a href="https://fastify.io/">Fastify web framework</a>, built to support CRUD (Create, Read, Update, and Delete) functionality of community midwife, health visitor, and school nurse team email addresses in YDH\'s catchment area.',
+						'<a href="https://somersetft.nhs.uk/yeovilhospital/">Yeovil Hospital</a>\'s Community Contacts RESTful API, a Node.js application using the <a href="https://fastify.io/">Fastify web framework</a>, built to support CRUD (Create, Read, Update, and Delete) functionality of community midwife, health visitor, and school nurse team email addresses in Yeovil Hospital\'s catchment area.',
 					contact: {
 						name: "Author",
 						email: "frazer.smith@somersetft.nhs.uk",
@@ -244,10 +244,9 @@ async function getConfig() {
 					version,
 					// Redoc specific extension to support loading image in docs
 					"x-logo": {
-						url: "/public/images/ydh-y-logo-transparent-background-wide-canvas.png",
-						backgroundColor: "#6D3176",
-						altText:
-							"Yeovil District Hospital NHS Foundation Trust logo",
+						url: "/public/images/sft-s-logo-transparent-background-wide-canvas.png",
+						backgroundColor: "#005EB8",
+						altText: "Somerset NHS Foundation Trust Logo",
 					},
 				},
 				// Components object populated by shared schemas at launch
